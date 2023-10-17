daf5d837e17ac6272831c9af4402e1ab13800a1b
donschaurich <leoschaurich.pf@outlook.com>
2023-10-16 22:35:03 -0300

    Arquivos enviados

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const number1 = parseFloat(window.prompt('Digite o número 1'))
+const number2 = parseFloat(window.prompt('Digite o número 2'))
+
+console.log("A soma é " + (parseFloat(number1) + parseFloat(number2)))
+console.log("A diferença é " + (number1 - number2))
+console.log("A multiplicação é " + (number1 * number2))
+console.log("A divisão é " + (number1 / number2))
